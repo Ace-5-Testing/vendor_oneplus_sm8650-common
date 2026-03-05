78acdc878073d25e70cd7c74ed9290c73cf0133f
realahnet <syedmusicrealise@gmail.com>
2026-03-06 05:36:08 +1300

    sm8650-common: Fixup widevine L1
    * Import liboemcrypto
    * Set missing prop from stock
    
    * Note: L1 needs Tee to be reprogrammed inorder for it to work. Hence ship KmInstallKeybox from stock so users can do so.
    
    Change-Id: Iab9ce95dcac3d11ad0f26735413fbd52ef1df3eb

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -810,6 +810,7 @@ PRODUCT_PACKAGES += \
     libdolbyeglcore \
     libdolbyencoderprocessor \
     libdolbyottcameracontrol \
+    liboemcrypto \
     libolc_vnd \
     liboplus_service \
     libosenseaidlhalclient \
