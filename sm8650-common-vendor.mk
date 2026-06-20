e0669a0e137f7362a0fe49fbd149c4a47cc5111c
Bruno Martins <bgcngm@gmail.com>
2026-06-20 17:40:28 +0000

    sm8650-common: De-blob oplus interfaces
    Change-Id: I86c24df31b72a9058ba3f2430f5c1bb469c4fd0b

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -1172,9 +1172,6 @@ PRODUCT_PACKAGES += \
     vendor.libdpmframework \
     vendor.libdpmtcm \
     vendor.libmwqemiptablemgr \
-    vendor.oplus.hardware.appradioaidl-V1-ndk \
-    vendor.oplus.hardware.communicationcenter-V2-ndk_vendor \
-    vendor.oplus.hardware.ims-V1-ndk \
     vendor.qti.ImsRtpService-V1-ndk_vendor \
     vendor.qti.ImsRtpService-aidl-Impl \
     vendor.qti.data.factoryservice-V1-ndk \
@@ -1318,11 +1315,6 @@ PRODUCT_PACKAGES += \
     libsubsys-service \
     libsubsys-utils \
     libvasapi \
-    vendor.oplus.hardware.appradio@1.0 \
-    vendor.oplus.hardware.communicationcenter-V1-ndk \
-    vendor.oplus.hardware.communicationcenter-V2-ndk \
-    vendor.oplus.hardware.subsys-V5-ndk \
-    vendor.oplus.hardware.subsys_radio-V9-ndk \
     vendor.qti.hardware.soter@1.0 \
     CACertService \
     CneApp \
